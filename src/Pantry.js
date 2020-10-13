120f2b39fdba499a967cd3a49bec660d3f703d24
Jordon Smith <jordon00smith@gmail.com>
2020-10-13 16:19:34 -0600

    Update Pantry and Pantry-test

--- src/Pantry.js
+++ src/Pantry.js
@@ -1,7 +1,36 @@
+const ingredients = require('../data/ingredients');
+const ingredientsData = ingredients.ingredientsData;
+
 class Pantry {
-    constructor() {
-        
+    constructor(userPantry) {
+        this.userPantry = userPantry;
     }
+    checkStock(recipe) {
+        recipe.ingredients.forEach(ingredient => {
+            const id = ingredient.id;
+            this.userPantry.forEach(pantryIngredient => {
+                if (pantryIngredient.ingredient === id) {
+                    recipe.ingredients.splice(ingredient, 1);
+                }
+            })
+        })
+        return recipe.ingredients;
+    }
+    listMissingIngredients(recipe) {
+        this.checkStock(recipe);
+        if (recipe.ingredients.length > 0) {
+            recipe.ingredients.forEach(ingredient => {
+                const id = ingredient.id;
+                ingredientsData.forEach(ingredientData => {
+                    if (ingredientData.id === id) {
+                        ingredient.name = ingredientData.name;
+                        ingredient.estimatedCostInCents = ingredientData.estimatedCostInCents;
+                    }
+                })
+            })
+        }
+        return recipe.ingredients;
+    }  
 }
 
 module.exports = Pantry;
--- test/Pantry-test.js
+++ test/Pantry-test.js
@@ -2,6 +2,8 @@ const chai = require('chai');
 const expect = chai.expect;
 
 const Pantry = require('../src/Pantry');
+const User = require('../src/User');
+const Recipe = require('../src/Recipe');
 
 describe('Pantry', () => {
     it('should be a function', () => {
@@ -12,5 +14,123 @@ describe('Pantry', () => {
         const pantry = new Pantry();
         
         expect(pantry).to.be.an.instanceof(Pantry);
+    });
+    
+    it('should have a user pantry', () => {
+        const currentPantry = [  {
+        "ingredient": 11477,
+        "amount": 4
+      },
+      {
+        "ingredient": 11297,
+        "amount": 4
+      }]
+        const user = new User('Joe', 1, currentPantry);
+        const pantry = new Pantry(user.pantry);
+        
+        expect(pantry.userPantry).to.deep.equal(user.pantry);
+    });
+    
+    it('should check that a users pantry has the correct ingredients', () => {
+        const currentPantry = [  {
+        "ingredient": 11477,
+        "amount": 4
+      },
+      {
+        "ingredient": 11297,
+        "amount": 4
+      }]
+        const ingredients = [
+        {
+          "id": 11477,
+          "quantity": {
+            "amount": 15,
+            "unit": "c"
+          }
+        },
+        {
+          "id": 18372,
+          "quantity": {
+            "amount": 5,
+            "unit": "tsp"
+          }
+        }];
+        const instructions = [
+          {
+            "instruction": "Butter the bread",
+            "number": 1
+          },
+          {
+            "instruction": "Toast the buttered bread",
+            "number": 2
+          }];
+        const recipe = new Recipe(123456, 'https://something.jpg', ingredients, instructions, 'Toast', ['breakfast', 'brunch']);
+        const user = new User('Joe', 1, currentPantry);
+        const pantry = new Pantry(user.pantry);
+        
+        pantry.checkStock(recipe);
+        
+        expect(pantry.checkStock(recipe)).to.deep.equal([recipe.ingredients[0]]);
+    });
+    
+    it('should list missing ingredients after checking pantry', () => {
+        const currentPantry = [  {
+        "ingredient": 11477,
+        "amount": 4
+      },
+      {
+        "ingredient": 11297,
+        "amount": 4
+      }]
+        const ingredients = [
+        {
+          "id": 11477,
+          "quantity": {
+            "amount": 15,
+            "unit": "c"
+          }
+        },
+        {
+          "id": 18372,
+          "quantity": {
+            "amount": 5,
+            "unit": "tsp"
+          }
+        }];
+        const instructions = [
+          {
+            "instruction": "Butter the bread",
+            "number": 1
+          },
+          {
+            "instruction": "Toast the buttered bread",
+            "number": 2
+          }];
+        const ingredientData = [
+        {
+          "id": 20081,
+          "name": "wheat flour",
+          "estimatedCostInCents": 142
+        },
+        {
+          "id": 18372,
+          "name": "bicarbonate of soda",
+          "estimatedCostInCents": 582
+        }];
+        const recipe = new Recipe(123456, 'https://something.jpg', ingredients, instructions, 'Toast', ['breakfast', 'brunch']);
+        const user = new User('Joe', 1, currentPantry);
+        const pantry = new Pantry(user.pantry);
+        
+        pantry.listMissingIngredients(recipe);
+        
+        expect(pantry.listMissingIngredients(recipe)).to.deep.equal([{
+          "id": 18372,
+          "quantity": {
+            "amount": 5,
+            "unit": "tsp"
+          },
+          "name": "bicarbonate of soda",
+          "estimatedCostInCents": 582
+        }]);
     });
 });
