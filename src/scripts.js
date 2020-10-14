875794275c0d92b7013145484919f9f434be2dfa
Jordon Smith <jordon00smith@gmail.com>
2020-10-14 16:45:10 -0600

    Get recipes to display on page when page loads

--- src/scripts.js
+++ src/scripts.js
@@ -1,1 +1,21 @@
-console.log('Hello world');
+let recipeCards = document.querySelector('.all-recipes');
+
+window.addEventListener('load', displayAllRecipes);
+
+function displayAllRecipes() {
+    recipeData.forEach(recipe => {
+        let recipeCard = `
+          <article class="recipe-card">
+            <div class="view-recipe">
+              <img src=${recipe.image} class="recipe-image">
+            </div>
+            <h4>${recipe.name}</h4>
+            <div class="recipe-card-buttons">
+              <button class="recipe-button"><img src="https://image.flaticon.com/icons/png/512/14/14815.png" class="recipe-button-image"></button>
+              <button class="recipe-button"><img src ="https://img.icons8.com/ios/452/chef-hat.png" class="recipe-button-image"></button>
+                <br>
+            </div>
+          </article>`
+    recipeCards.insertAdjacentHTML('beforeend', recipeCard);
+    })
+}
