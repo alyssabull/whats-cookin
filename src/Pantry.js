6529ef185c7b609c5ef8e32afc0dcb1e1814f4f6
Alyssa Bull <alyssabull@gmail.com>
2020-10-21 18:36:45 -0600

    Fix spacing and identation in class files

--- src/Pantry.js
+++ src/Pantry.js
@@ -18,20 +18,20 @@ class Pantry {
     this.missingIngredients = [];
     recipe.ingredients.forEach(ingredient => {
       const id = ingredient.id;
-          if (!this.missingIngredients.includes(ingredient)) {
-            return this.missingIngredients.push(ingredient);
-          }
+        if (!this.missingIngredients.includes(ingredient)) {
+          return this.missingIngredients.push(ingredient);
+        }
     })
     this.userPantry.forEach(pantryIngredient => {
-        let id = pantryIngredient.ingredient;
+      let id = pantryIngredient.ingredient;
         this.missingIngredients.forEach(ingredient => {
-            if (id === ingredient.id) {
-                let ingredientIndex = this.missingIngredients.indexOf(ingredient);
-                return this.missingIngredients.splice(ingredientIndex, 1);
-            }
-        })
+          if (id === ingredient.id) {
+            let ingredientIndex = this.missingIngredients.indexOf(ingredient);
+            return this.missingIngredients.splice(ingredientIndex, 1);
+          }
+      })
     })
-      return this.missingIngredients;
+    return this.missingIngredients;
   }
 }
 
